80f5c7ca35f4d3e8ffc53cae2cf2d2902b187c88
kamami <69190381+kamami-src@users.noreply.github.com>
2021-06-16 13:53:25 +0900

    画面遷移セクションまで

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,15 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  const history = useHistory();
+  console.log(history);
+  const onClickBack = () => history.goBack();
   return (
     <div>
-      {state.length}
+      {state}
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
